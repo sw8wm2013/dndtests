a377bfb34d16023089a1a2c1f32f16b26cea4924
Samantha Wessel <swessel@npr.org>
2020-05-04 17:21:14 -0400

    Tried another implementation of autoscroll

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,10 @@
 
-import { Component, OnInit, Input, Output, EventEmitter, Optional } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, Optional, ElementRef, ViewChild } from '@angular/core';
 
 import { SortableSpecService, CardList, Card } from '../spec';
 import { SkyhookSortableRenderer } from '@angular-skyhook/sortable';
 
+import autoScroll from 'dom-autoscroller';
 
 @Component({
   selector: 'app-list',
@@ -16,6 +17,10 @@ export class ListComponent  {
   @Input() preview = false;
   @Output() addCard = new EventEmitter<string>();
 
+  @ViewChild('card') card: ElementRef;
+  scrollElements: any;
+  cardScroll: any;
+
   // we won't use these, but you can listen to any old monitor state if you like.
   // there is a shortcut for m.isDragging() for use in a template, called render?.isDragging$
   placeholder$ = this.render && this.render.source.listen(m => m.isDragging());
@@ -27,9 +32,38 @@ export class ListComponent  {
   constructor(
       public specs: SortableSpecService,
       @Optional() public render: SkyhookSortableRenderer<CardList>,
+      private elRef: ElementRef,
   ) { }
 
   trackById = (_: any, x: Card) =>  x.id;
+
+
+  ngAfterViewInit(){
+    console.log(' BOARD COMPONENT AFTER INIT');
+    setTimeout(() => { this.initializeScroll(); }, 500);
+  };
+
+  initializeScroll(){
+    console.log('INITALIZING SCROLL ON BOARD COMPONENT');
+    // this.cardScroll = Array.from(this.elRef.nativeElement.querySelectorAll('.card-handle'));
+    this.cardScroll = this.card;
+    console.log('CARD SCROLL', this.card);
+    this.scrollElements = autoScroll([...this.cardScroll], {
+      margin: 100,
+      speed: 800,
+      scrollWhenOutside: false,
+      autoScroll: function(){
+        console.log('scrolling board comp');
+        return this.down;
+      }
+    });
+  }
+
+  resetScroll(){
+    [...this.cardScroll].forEach(el => this.scrollElements.remove(el));
+    this.cardScroll = Array.from(this.elRef.nativeElement.querySelectorAll('.list-wrapper'));
+    [...this.cardScroll].forEach(el => this.scrollElements.add(el));
+  }
 
 
 }
